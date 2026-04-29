c207d4ba6ea4e6072ff418fda940d5ee54cf4c19
Sakshi Landage <sakshilandage84@gmail.com>
2026-04-29 13:31:47 +0530

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+// "use strict"; // treat all JS code as newer version 
+
+// alert(3+3) --> we are using node.js, not a brower
+// alert("Thank You.")
+
+console.log(3
+    + 3
+)               // code readability should be high
+
+// Datatypes
+
+let name = "Sakshi"
+let age = 23
+let isLoggedIn = false
+
+// number
+// bigint
+// string
+// boolean
+// null (standalone value)
+// undefined (value is not assign till now)
+// symbol (react -> uniqueness find)
+
+
+// object
+
+console.log(typeof "sakshi");   // string
+
+console.log(typeof null);     // object
+
+console.log(typeof undefined);     // undefined
+
