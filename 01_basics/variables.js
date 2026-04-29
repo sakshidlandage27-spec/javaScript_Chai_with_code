31e2d7758291bb436d0b002874997a48ee5095b1
Sakshi Landage <sakshilandage84@gmail.com>
2026-04-29 12:58:39 +0530

    Learning about Variables in JavaScript

---
+++ 01_basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 14455
+let accountEmail = "sakshi@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Nashik"
+
+let accountState;
+
+// accountId=101
+
+console.log(accountId);
+
+accountEmail = "sakshilandage@gmail.com"
+accountPassword = "87654321"
+accountCity = "Bengaluru"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not use var keywords
+because of issue in block scope and functional scope
+*/
